ba941b0564a010b8236af1469112ad83310c0ed9
Nicholas Skinsacos <nskins@umich.edu>
2022-08-24 17:21:00 -0400

    Update test case w/ name of app

--- test/todo_web/controllers/page_controller_test.exs
+++ test/todo_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TodoWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Todo!"
   end
 end
