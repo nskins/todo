79ea989cbac25286b96674b58462ed754413a79a
Nicholas Skinsacos <nskins@umich.edu>
2022-09-02 17:02:12 -0400

    When logged in, redirect to /items

--- test/todo_web/controllers/user_registration_controller_test.exs
+++ test/todo_web/controllers/user_registration_controller_test.exs
@@ -14,7 +14,7 @@ defmodule TodoWeb.UserRegistrationControllerTest do
 
     test "redirects if already logged in", %{conn: conn} do
       conn = conn |> log_in_user(user_fixture()) |> get(Routes.user_registration_path(conn, :new))
-      assert redirected_to(conn) == "/"
+      assert redirected_to(conn) == "/items"
     end
   end
 
@@ -29,7 +29,7 @@ defmodule TodoWeb.UserRegistrationControllerTest do
         })
 
       assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+      assert redirected_to(conn) == "/items"
 
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
