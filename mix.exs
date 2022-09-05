aa015ec8aaaf3148c8b8b66a2adfa9a66015b49d
Nicholas Skinsacos <nskins@umich.edu>
2022-09-05 11:05:24 -0400

    Add Tailwind as dependency

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Todo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev}
     ]
   end
 
