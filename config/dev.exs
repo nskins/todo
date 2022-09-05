0ec4d2b1a0453aeeadec62feeb3c59b4d71e3582
Nicholas Skinsacos <nskins@umich.edu>
2022-09-05 11:07:55 -0400

    Enable watch mode for Tailwind in dev environment

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :todo, TodoWeb.Endpoint,
   secret_key_base: "zgCG2qJx8Dc10XSJKau1OP1lHjbmRNIAdHhS8/ZxabHL2d/Q3fm4rZbmnj3tN7jZ",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
