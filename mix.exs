1a8397123fcd1d84f4ab57f18af1b73492aa6cab
Nicholas Skinsacos <nskins@umich.edu>
2022-09-05 11:09:11 -0400

    Run tailwind for deployment assets

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Todo.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
