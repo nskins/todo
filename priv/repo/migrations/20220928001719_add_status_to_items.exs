1160640c60b18b6455898dcd149278b1af677c81
Nicholas Skinsacos <nskins@umich.edu>
2022-09-28 15:59:21 -0400

    Update migration to add status field to items table

--- priv/repo/migrations/20220928001719_add_status_to_items.exs
+++ priv/repo/migrations/20220928001719_add_status_to_items.exs
@@ -2,6 +2,8 @@ defmodule Todo.Repo.Migrations.AddStatusToItems do
   use Ecto.Migration
 
   def change do
-
+    alter table(:items) do
+      add :status, :string, default: "todo", null: false
+    end
   end
 end
