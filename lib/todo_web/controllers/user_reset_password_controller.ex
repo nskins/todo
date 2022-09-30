6de9b33dc6edabf11e01aee66186ed4aae071fd6
Josh Alling <joshralling@gmail.com>
2022-09-29 22:40:16 -0700

    style reset password forms

--- lib/todo_web/controllers/user_reset_password_controller.ex
+++ lib/todo_web/controllers/user_reset_password_controller.ex
@@ -3,10 +3,11 @@ defmodule TodoWeb.UserResetPasswordController do
 
   alias Todo.Accounts
 
+  plug :put_layout, {TodoWeb.LayoutView, "auth_form.html"}
   plug :get_user_by_reset_password_token when action in [:edit, :update]
 
   def new(conn, _params) do
-    render(conn, "new.html")
+    render(conn, "new.html", error_message: nil, title: "Forgot your password?")
   end
 
   def create(conn, %{"user" => %{"email" => email}}) do
@@ -26,7 +27,10 @@ defmodule TodoWeb.UserResetPasswordController do
   end
 
   def edit(conn, _params) do
-    render(conn, "edit.html", changeset: Accounts.change_user_password(conn.assigns.user))
+    render(conn, "edit.html",
+      changeset: Accounts.change_user_password(conn.assigns.user),
+      title: "Reset password"
+    )
   end
 
   # Do not log in the user after reset password to avoid a
