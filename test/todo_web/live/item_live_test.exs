a6b3ed733dbf942e2e9fbbfb3a41d485e60bf022
Ben <bflatau@gmail.com>
2022-10-06 22:55:00 -0700

    removed show page template, router references, show files, and updated tests

--- test/todo_web/live/item_live_test.exs
+++ test/todo_web/live/item_live_test.exs
@@ -78,37 +78,4 @@ defmodule TodoWeb.ItemLiveTest do
       refute has_element?(index_live, "#item-#{item.id}")
     end
   end
-
-  describe "Show" do
-    setup [:create_item]
-
-    test "displays item", %{conn: conn, item: item} do
-      {:ok, _show_live, html} = live(conn, Routes.item_show_path(conn, :show, item))
-
-      assert html =~ "Show Item"
-      assert html =~ item.description
-    end
-
-    test "updates item within modal", %{conn: conn, item: item} do
-      {:ok, show_live, _html} = live(conn, Routes.item_show_path(conn, :show, item))
-
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit Item"
-
-      assert_patch(show_live, Routes.item_show_path(conn, :edit, item))
-
-      assert show_live
-             |> form("#item-form", item: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        show_live
-        |> form("#item-form", item: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.item_show_path(conn, :show, item))
-
-      assert html =~ "Item updated successfully"
-      assert html =~ "some updated description"
-    end
-  end
 end
