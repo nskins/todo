0f19bfce7ba0e56beb74e45d93e7ee1343e39c5d
chinmaym07 <>
2022-10-29 13:59:56 +0530

    Added Custom Code to do Date Comparision & render different color on the basis of due date

--- lib/todo_web/live/item_live/item_component.ex
+++ lib/todo_web/live/item_live/item_component.ex
@@ -1,4 +1,13 @@
 defmodule ItemComponent do
   use TodoWeb, :live_component
-  alias Todo.Notebook.Item
+  require Date
+  alias Todo.Notebook.Item  
+  def compare_date(due_date) do 
+    val = Date.compare(due_date, DateTime.utc_now())
+    case val do
+      :lt -> -1
+      :gt -> 1
+      :eq -> 0
+    end
+  end
 end
