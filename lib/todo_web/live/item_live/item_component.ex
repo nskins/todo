be38d5bd2b74e498055392e0933592adb8cc444a
chinmaym07 <>
2022-10-30 11:11:06 +0530

    Refactored code

--- lib/todo_web/live/item_live/item_component.ex
+++ lib/todo_web/live/item_live/item_component.ex
@@ -1,13 +1,12 @@
 defmodule ItemComponent do
   use TodoWeb, :live_component
-  require Date
   alias Todo.Notebook.Item  
   def compare_date(due_date) do 
     val = Date.compare(due_date, DateTime.utc_now())
     case val do
-      :lt -> -1
-      :gt -> 1
-      :eq -> 0
+      :lt -> "font-bold text-red-500"
+      :eq -> "font-bold text-orange-500"
+      :gt -> ""
     end
   end
 end
