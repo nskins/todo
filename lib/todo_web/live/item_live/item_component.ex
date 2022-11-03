7759166452291fef507c373296af6f38055242a8
Nicholas <nskins@umich.edu>
2022-11-02 20:51:34 -0400

    Perform due date comparison with local time (#42)
    * Install Timex package dependency
    
    * Use local date for due date comparison
    
    * Run 'mix format'

--- lib/todo_web/live/item_live/item_component.ex
+++ lib/todo_web/live/item_live/item_component.ex
@@ -2,8 +2,8 @@ defmodule ItemComponent do
   use TodoWeb, :live_component
   alias Todo.Notebook.Item
 
-  def compare_date(due_date) do
-    val = Date.compare(due_date, DateTime.utc_now())
+  def compare_date(due_date, local_date) do
+    val = Date.compare(due_date, local_date)
 
     case val do
       :lt -> "font-bold text-red-500"
--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Todo.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
+      {:timex, "~> 3.7"}
     ]
   end
 
