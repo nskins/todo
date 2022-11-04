43a75ea132384ade13e9d407bb2272b335c67fe6
Nicholas <nskins@umich.edu>
2022-11-03 20:28:33 -0400

    Implement migration by adding timezone to users table

--- priv/repo/migrations/20221104001712_add_timezone_to_users.exs
+++ priv/repo/migrations/20221104001712_add_timezone_to_users.exs
@@ -2,6 +2,8 @@ defmodule Todo.Repo.Migrations.AddTimezoneToUsers do
   use Ecto.Migration
 
   def change do
-
+    alter table(:users) do
+      add :timezone, :string, null: true
+    end
   end
 end
