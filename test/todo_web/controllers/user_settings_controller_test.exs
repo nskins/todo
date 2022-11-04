d2abb83a2177dcfaf81e0b2fd6576ea42521992c
Nicholas <nskins@umich.edu>
2022-11-04 17:00:50 -0400

    Write a test verifying user timezone does not get updated when an invalid password is submitted

--- test/todo_web/controllers/user_settings_controller_test.exs
+++ test/todo_web/controllers/user_settings_controller_test.exs
@@ -91,7 +91,7 @@ defmodule TodoWeb.UserSettingsControllerTest do
   
   describe "PUT /users/settings (change timezone form)" do
     test "updates the user timezone and resets tokens", %{conn: conn, user: user} do
-      new_password_conn =
+      new_conn =
         put(conn, Routes.user_settings_path(conn, :update), %{
           "action" => "update_timezone",
           "current_password" => valid_user_password(),
@@ -100,13 +100,34 @@ defmodule TodoWeb.UserSettingsControllerTest do
           }
         })
 
-      assert redirected_to(new_password_conn) == Routes.user_settings_path(conn, :edit)
-      assert get_session(new_password_conn, :user_token) != get_session(conn, :user_token)
-      assert get_flash(new_password_conn, :info) =~ "Timezone updated successfully"
+      assert redirected_to(new_conn) == Routes.user_settings_path(conn, :edit)
+      assert get_session(new_conn, :user_token) != get_session(conn, :user_token)
+      assert get_flash(new_conn, :info) =~ "Timezone updated successfully"
       
       new_user = Accounts.get_user_by_email(user.email)
 
       assert new_user.timezone == "America/Phoenix"
+    end
+
+    test "does not update timezone on invalid data", %{conn: conn, user: user} do
+      new_conn =
+        put(conn, Routes.user_settings_path(conn, :update), %{
+          "action" => "update_timezone",
+          "current_password" => "invalid",
+          "user" => %{
+            "timezone" => "America/Phoenix",
+          }
+        })
+
+      response = html_response(new_conn, 200)
+      assert response =~ "<h1>Settings</h1>"
+      assert response =~ "is not valid"
+
+      assert get_session(new_conn, :user_token) == get_session(conn, :user_token)
+
+      new_user = Accounts.get_user_by_email(user.email)
+
+      assert new_user.timezone == nil
     end
   end
 
